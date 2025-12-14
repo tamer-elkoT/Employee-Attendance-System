eae96153bb59d968717396d7ca7f509115ff941a
tamer-elkoT <tamerelkottrinx1.4@gmail.com>
2025-12-14 13:51:57 +0200

    Test the importing of configration setting for the DB URL and FR Tolerance

---
+++ app/__init__.py
@@ -0,0 +1,3 @@
+# This file is empty.
+# Its only purpose is to tell Python that this folder ("app") should be treated as a package.
+# This allows us to use imports like "from app.models import Employee".
---
+++ app/config.py
@@ -0,0 +1,19 @@
+#This file reads the raw text from .env and converts it into usable Python variables.
+
+import os
+from dotenv import load_dotenv # Load environment variables from a .env file
+
+load_dotenv() # load contents of the .env file into the environment
+
+# Application Settings
+
+class Settings:
+    # Get the DB URL from the environment variable , if it's missing in .env, use a default value provided.
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "sqlite:///./attendance.db")
+    # Get tolerance. Crucially, wrap it in float() because environment variables are always strings of text.
+    FACE_TOLRANCE: float = float(os.getenv("FACE_TOLERANCE",0.5))
+
+
+settings = Settings()
+
+
